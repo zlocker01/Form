8a92593274dde899773cf2fa3a15dd9229fb8ad2
Diego Beristain de la Mora <121736405+zlocker01@users.noreply.github.com>
2023-03-06 11:00:49 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,114 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const email = {
+        email: '',
+        asunto: '',
+        mensaje: ''
+    };
+    /* variables */
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
+    const btnSubmit = document.querySelector('#formulario button[type="submit"]')
+    const btnReset = document.querySelector('#formulario button[type="reset"]')
+    const spinner = document.querySelector('#spinner');
+
+
+    /* asignar eventos */
+    inputEmail.addEventListener('input', validar);
+    inputAsunto.addEventListener('input', validar);
+    inputMensaje.addEventListener('input', validar);
+    formulario.addEventListener('submit', enviarEmail);
+    btnReset.addEventListener('click', function(e){
+        e.preventDefault();
+        //reiniciar el objeto
+        resetFormulario();
+    });
+
+
+    /* functions */
+    function enviarEmail(e){
+        e.preventDefault();
+        spinner.classList.add('flex');
+        spinner.classList.remove('hidden');
+        setTimeout(()=>{
+            spinner.classList.add('hidden');
+            email.email = '';
+            resetFormulario();
+            //crear una alerta de exito
+            const alertaExito = document.createElement('P');
+            alertaExito.classList.add('bg-green-500', 'text-white', 'p-2', 'text-center', 'rounded-lg', 'mt-10', 'font-bold', 'text-sm', 'uppercase');
+            alertaExito.textContent = 'Mensaje ENVIADO con exito'
+            formulario.appendChild(alertaExito);
+            //ocualtar el mensaje de exito
+            setTimeout(()=>{
+                alertaExito.remove();
+            },3000)
+        }, 3000);
+    };
+    function validar(e){
+        if(e.target.value.trim() === ''){
+            mostrarAlerta(`El ${e.target.id} es obligatorio`, e.target.parentElement);
+            email[e.target.name] = '';
+            comprobarEmail();
+            return;
+        }
+
+        if(e.target.id === 'email' && !validarEmail(e.target.value)){
+            mostrarAlerta('El email no es valido', e.target.parentElement);
+            email[e.target.name] = '';
+            comprobarEmail();
+            return;
+        };
+
+        limpiarAlerta(e.target.parentElement);
+
+        /* asignar los valores al objeto */
+        email[e.target.name] = e.target.value.trim().toLowerCase();
+
+        /* comprobar email */
+        comprobarEmail();
+    };
+
+    function mostrarAlerta(mensaje, referencia){
+        /* comprobar is existe un alaerta previa en el form */
+        limpiarAlerta(referencia);
+        /* generar el aviso en html */
+        const error = document.createElement('P');
+        error.textContent = mensaje;
+        error.classList.add('bg-red-600', 'text-white', 'text-center')
+        /* agregar error al div padre que contiene el input */
+        referencia.appendChild(error);
+    };
+
+    function limpiarAlerta(referencia){
+        const alerta = referencia.querySelector('.bg-red-600');
+        if(alerta){
+            alerta.remove();
+        };
+    };
+
+    function validarEmail(email){
+        const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+        const resultado = regex.test(email);
+        return resultado;
+    };
+
+    function comprobarEmail(){
+        if(Object.values(email).includes('')){
+            btnSubmit.classList.add('opacity-50');
+            btnSubmit.disabled = true;
+            return;
+        }
+            btnSubmit.classList.remove('opacity-50');
+            btnSubmit.disabled = false;
+    };
+
+    function resetFormulario(){
+        email.email = '';
+        email.asunto = '';
+        email.mensaje = '';
+        formulario.reset();
+        comprobarEmail();
+    };
+});
